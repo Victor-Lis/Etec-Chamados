df9e6a8e638eebc683a79d72b1f8d791a0ba0182
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-06-05 23:08:12 -0300

    Finalizando gráfico

--- src/app/chamados/analise/page.tsx
+++ src/app/chamados/analise/page.tsx
@@ -1,7 +1,38 @@
-export default function Analise() {
- return (
-   <div>
+import { formatDate } from '@/utils/formatDate';
+import { getTickets } from './../utils/db';
+import LineGraph from './LineGraph';
+import { Dataset } from '@/@types/dataset';
 
-   </div>
- );
+type TicketCount = {
+  date: string;
+  count: number;
+};
+
+export default async function Analise() {
+  let tickets = await getTickets();
+
+  // Group tickets by formatted start date
+  const ticketsGroupedByDate = tickets.reduce((acc, ticket) => {
+    if (ticket.time_end) {
+      const formattedDate = formatDate(ticket.time_start);
+      acc[formattedDate] = (acc[formattedDate] || 0) + 1;
+    }
+    return acc;
+  }, {} as Record<string, number>);
+
+  // Convert grouped data to chart-friendly format
+  const labels: string[] = Object.keys(ticketsGroupedByDate);
+  const datasets: Dataset[] = [
+    {
+      label: 'Tickets Atendidos', // Assuming "atendido" means "attended"
+      borderColor: '#00ff00',
+      data: Object.values(ticketsGroupedByDate),
+    },
+  ];
+
+  return (
+    <div>
+      <LineGraph data={{ labels, datasets }} />
+    </div>
+  );
 }
