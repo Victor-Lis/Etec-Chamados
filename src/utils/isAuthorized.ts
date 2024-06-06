73c9eceee7652c1f03d8397dfde90da7230c85be
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-06-05 23:12:56 -0300

    "Desativando sistema de trava de rotas para testes do professor"

--- src/utils/isAuthorized.ts
+++ src/utils/isAuthorized.ts
@@ -8,13 +8,13 @@ import { redirect } from "next/navigation";
 
 export async function isAuthorized(){
     "use server"
-    let sessionUser = await getServerSession(authOptions);
-    console.log("sessionUser", sessionUser)
-    if(!!!sessionUser) return redirect("/")
-    let user = await prisma.atendentes.findFirst({
-        where: {
-            email: sessionUser?.user?.email as string
-        }
-    })
-    if(!!!user) return redirect("/")
+    // let sessionUser = await getServerSession(authOptions);
+    // console.log("sessionUser", sessionUser)
+    // if(!!!sessionUser) return redirect("/")
+    // let user = await prisma.atendentes.findFirst({
+    //     where: {
+    //         email: sessionUser?.user?.email as string
+    //     }
+    // })
+    // if(!!!user) return redirect("/")
 }
